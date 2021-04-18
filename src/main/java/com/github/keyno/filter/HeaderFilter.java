ffbc38d03b409b592a040f640d202e5ac4f016c7
Keyno <kofujiw@gmail.com>
2021-04-19 01:49:01 +0900

    中身の実装(仮)

--- src/main/java/com/github/keyno/filter/HeaderFilter.java
+++ src/main/java/com/github/keyno/filter/HeaderFilter.java
@@ -1,14 +1,26 @@
 package com.github.keyno.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
 
 @WebFilter(
         urlPatterns = {"/*"},
         asyncSupported = true
 )
 public class HeaderFilter implements Filter {
+
+    private final String TARGET_COOKIE_NAME = "target-cookie";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HeaderFilter.class);
 
     @Override
     public final void init(final FilterConfig filterConfig) throws ServletException {
@@ -20,8 +32,22 @@ public class HeaderFilter implements Filter {
             final ServletRequest request,
             final ServletResponse response,
             final FilterChain chain
-            ) {
+            ) throws IOException, ServletException {
+        Cookie[] cookies = Optional.ofNullable(((HttpServletRequest) request).getCookies())
+                .orElse(new Cookie[0]);
+        Cookie cookie = Arrays.stream(cookies)
+                .filter(cookie1 -> cookie1.getName().equals(TARGET_COOKIE_NAME))
+                .findFirst()
+                .orElse(null);
 
+        if (Objects.isNull(cookie)) {
+            LOGGER.debug("cookie is null");
+        } else {
+            LOGGER.debug("cookie is exist {}", cookie);
+        }
+
+        request.setAttribute("", cookie);
+        chain.doFilter(request, response);
     }
 
     @Override
